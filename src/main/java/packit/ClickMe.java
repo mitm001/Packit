1ed7371e9dfdc0d8532d0411cf3f80ed51574740
mitm001 <mitm001@users.noreply.github.com>
2020-08-05 17:08:48 -0700

    [test]: test appdata/local install

--- src/main/java/packit/ClickMe.java
+++ src/main/java/packit/ClickMe.java
@@ -1,16 +1,21 @@
 package packit;
 
+import com.sun.javafx.util.Utils;
 import java.util.List;
+import java.util.prefs.Preferences;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class ClickMe extends Application {
+
+    private String env;
     
     Button btn;
-    static String arg = null;
+    String arg = null;
     
     /**
      * @param args the command line arguments
@@ -22,7 +27,7 @@ public class ClickMe extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        
+        configurePreferences();
         Parameters parameters = getParameters();
         List<String> unnamed = parameters.getUnnamed();
         
@@ -36,9 +41,11 @@ public class ClickMe extends Application {
         btn.setText(arg == null ? "Click me please!": arg);
         btn.setOnAction(e -> buttonClick());
         
+        Label lbl = new Label(env);
+        
         BorderPane pane = new BorderPane();
         pane.setCenter(btn);
-        
+        pane.setTop(lbl);
         Scene scene = new Scene(pane, 300, 250);
         
         stage.setScene(scene);
@@ -52,5 +59,34 @@ public class ClickMe extends Application {
         } else {
             btn.setText(arg == null ? "Click me please!" : arg);
         }
+    }
+    
+        /**
+     * Method to set the registry settings for testing.
+     */
+    private void configurePreferences() {
+        
+        if (Utils.isWindows()) {
+            env = System.getenv("LOCALAPPDATA");
+//            env = System.getProperty("user.home");
+        } else if (Utils.isUnix()) {
+
+        } else if (Utils.isMac()) {
+            
+        }
+        
+        //Distribution registry
+//        Preferences pref = Preferences.userRoot().node("polygeddon").node("dist");
+//        pref = null;
+//        pref = Preferences.userRoot().node("polygeddon").node("dist").node("updater");
+//        pref.remove("startscript");
+//        pref.put("startscript", "C:/Users/Robert/Documents/PolyGameClient/Updater/bin/updater_startup.bat");
+//        pref.remove("parent");
+//        pref.put("parent", "C:/Users/Robert/Documents/PolyGameClient");
+//        pref = null;
+//        pref = Preferences.userRoot().node("polygeddon").node("dist").node("client");
+//        pref.remove("startscript");
+//        pref.put("startscript", "C:/Users/Robert/Documents/PolyGameClient/bin/client_startup.bat");
+        
     }
 }
